3f6abedbb27f1e5ab1a200d06050e8af9c148313
seolhaaa <shchu07@korea.ac.kr>
2022-01-31 21:00:51 +0900

    course 33

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,5 @@
+from audioop import reverse
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -13,16 +15,17 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:detail')
     template_name = 'profileapp/create.html'
 
-    # 임시 user 데이터!
-    # (다른 사람의 프로필을 만들 수 있는 가능성을 제거하기 위해 forms에 user 정보를 받지 않아 생기는 오류를 해결하기 위해
     def form_valid(self, form):
         temp_profile = form.save(commit=False)
         temp_profile.user = self.request.user
         temp_profile.save()
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -30,5 +33,7 @@ class ProfileUpdateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
