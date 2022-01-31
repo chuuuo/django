e54fd7b3170976945ebaebf827db2a251a576c49
seolhaaa <shchu07@korea.ac.kr>
2022-01-31 19:29:19 +0900

    error fix

--- profileapp/models.py
+++ profileapp/models.py
@@ -8,11 +8,3 @@ class Profile(models.Model):
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=20, unique=True, null=True)
     message = models.CharField(max_length=100, null=True)
-
-    def form_valid(self, form):
-        # 임시 user 데이터!
-        # (다른 사람의 프로필을 만들 수 있는 가능성을 제거하기 위해 forms에 user 정보를 받지 않아 생기는 오류를 해결하기 위해
-        temp_profile = form.save(commit=False)
-        temp_profile.user = self.request.user
-        temp_profile.save()
-        return super().form_valid(form)
--- profileapp/views.py
+++ profileapp/views.py
@@ -13,3 +13,11 @@ class ProfileCreateView(CreateView):
     form_class = ProfileCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
+
+    # 임시 user 데이터!
+    # (다른 사람의 프로필을 만들 수 있는 가능성을 제거하기 위해 forms에 user 정보를 받지 않아 생기는 오류를 해결하기 위해
+    def form_valid(self, form):
+        temp_profile = form.save(commit=False)
+        temp_profile.user = self.request.user
+        temp_profile.save()
+        return super().form_valid(form)
